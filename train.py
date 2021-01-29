d9291bca08664fca61b29737531173d4c58fbd1a
unknown <jyothiprakashpatnaik@gmail.com>
2021-01-29 23:40:04 +0530

    First Commit

---
+++ train.py
@@ -0,0 +1,12 @@
+# Charect recognization Gujarathi
+'''
+	1. Dataset collection
+	2. Cnn Design
+	3. Pre-processing 
+	4. Training 
+	5. loading model
+	6. pre-processing i/p
+	7. Classification
+'''
+import webbrowser
+webbrowser.open('https://colab.research.google.com/drive/1w1ooq93kRyR5qFdJapxdrMADRluma4v2#scrollTo=BjB_D7ySJ7ZE')
